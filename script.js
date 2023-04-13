b979fc67c40b66d6498bb79d85b309086798169e
Andrea Guarino <andrea@andreaguarino.dev>
2023-04-13 19:38:39 +0200

    First implementation

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+// Use Typed.js for typing effect in header
+var typed = new Typed("#typed", {
+  strings: ["Andrea Guarino", "Software Engineer"],
+  typeSpeed: 60,
+  loop: true,
+  loopCount: Infinity,
+});

baf08e39e16fc420387e453ddc6e668172561a8c
Andrea Guarino <andrea@andreaguarino.dev>
2023-04-15 17:21:50 +0200

    Edit homepage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 // Use Typed.js for typing effect in header
 var typed = new Typed("#typed", {
-  strings: ["Andrea Guarino", "Software Engineer"],
+  strings: ["Software Engineer"],
   typeSpeed: 60,
   loop: true,
   loopCount: Infinity,
